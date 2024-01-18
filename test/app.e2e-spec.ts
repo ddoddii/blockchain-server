b80e4a5f92a1c64a4a8e3011c7b859a14b8492a4
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-18 19:03:45 +0900

    ✅ Test : block_number test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+    let app: INestApplication;
+    let prisma: PrismaService;
+    let config: ConfigService;
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+            }),
+        );
+        await app.init();
+        await app.listen(3334);
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+        prisma = app.get(PrismaService);
+        pactum.request.setBaseUrl('http://localhost:3334');
+    });
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    afterAll(() => {
+        app.close();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    describe('Ethers', () => {
+        it('should return recent block number', () => {
+            return pactum
+                .spec()
+                .get('/ethers/block_number')
+                .expectStatus(200)
+                .expect((res) => {
+                    console.log(res.res.body);
+                    if (res.res.body.length !== 8)
+                        throw new Error(
+                            'Expected body to be an 8-digit block number',
+                        );
+                });
+        });
+    });
 });
