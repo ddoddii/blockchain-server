1a91ccbbd553e9fddb9b3815b37a09d42dffbb07
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-18 19:03:33 +0900

    🔒️ Fix : localhost port number

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { EthersController } from './ethers/ethers.controller';
+import { EthersService } from './ethers/ethers.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        PrismaModule,
+    ],
+    controllers: [EthersController],
+    providers: [AppService, EthersService, PrismaService],
 })
 export class AppModule {}
